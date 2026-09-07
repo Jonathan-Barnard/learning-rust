c91857602cf05b6e678b9eb8c67fbe0d895dd02e
Jonathan Barnard <j.ebarnard@proton.me>
2026-09-07 17:07:33 +0100

    alg

---
+++ algorithms/src/core/regression.rs
@@ -0,0 +1,10 @@
+
+#[cfg(test)]
+mod tests {
+    // use super::*;
+
+    #[test]
+    fn working(){
+        assert_eq!(2, 2);
+    }
+}
---
+++ algorithms/src/main.rs
@@ -0,0 +1,12 @@
+use algorithms::core::regression;
+
+use ndarray::prelude::*;
+
+
+fn linear_regression(X, y) -> (f64, f64) {
+    (4.5, 3.5)
+}
+
+fn main () {
+
+}
